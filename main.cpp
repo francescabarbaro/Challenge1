5f7939a417f74af1a3dd6a578b44f34b1c7b87fe
Francesca Barbaro <francesca.barbaro@mail.polimi.it>
2024-03-16 18:35:49 +0000

    vanilla code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
-
+#include <cmath>
+#include "gradient.hpp"
 
 int main (){
 
+//Define the function f and his gradient as lambdas functions
+    std::function<double(std::vector<double>)> f=[](std::vector<double> x){
+        return x[0]*x[1] + 4*std::pow(x[0],4) + std::pow(x[1],2) + 3*x[0];
+    };
+    
+    std::function<std::vector<double>(std::vector<double>)> grad=[](std::vector<double> x){
+        std::vector<double> gradient;
+
+        gradient.emplace_back(x[1] + 16*std::pow(x[0],3) + 3);
+        gradient.emplace_back(x[0] + 2*x[1]);
+
+        return gradient;
+    };
+
+
+//Assign values to my structure of data
+    Data data;
+    data.f = f;
+    data.grad = grad;
+    data.start_point = {0.0,0.0};
+    data.epsilon_step = std::pow(10,-6);
+    data.epsilon_res = std::pow(10,-6);
+    data.initial_step = 1;
+    data.maxit = 1000;
+    data.sigma = 0.3;
+
+    // find the minimum point in the function f
+    std::vector<double> minimum = data.start_point;
+    minimum = gradient_method(data);
+
+    std::cout << "the minimum point is: ";
+    for(std::size_t i=0; i<minimum.size(); ++i)
+        std::cout<<minimum[i]<<"  "<<std::endl;
     return 0;
+
+
+
+
+    return 0;
+
 }
